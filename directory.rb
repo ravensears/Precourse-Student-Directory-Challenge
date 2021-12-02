89fc833ce947c1e270151ed822c1e7fff6d37776
Raven Sears <ravenrosereynolds@gmail.com>
2021-12-02 11:20:15 +0000

    Updates to Interactive Student Directory

--- directory.rb
+++ directory.rb
@@ -1,3 +1,8 @@
+def print_header
+    puts "The students of Villians Academy"
+    puts "-------------"
+  end
+
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
@@ -17,36 +22,10 @@ def input_students
     students
   end  
 
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-]
-
-def print_header
-  puts "The students of Villians Academy"
-  puts "-------------"
-end
-
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
 print_header
-print(students)
+students = input_students
 print_footer(students)
