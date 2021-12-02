e742845580561a5baff073c3aa108f8a7078b7c7
Raven Sears <ravenrosereynolds@gmail.com>
2021-12-02 10:27:03 +0000

    Interactive Student Library

--- directory.rb
+++ directory.rb
@@ -1,3 +1,22 @@
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    # create an empty array
+    students = []
+    # get the first name
+    name = gets.chomp
+    # while the name is not empty, repeat this code
+    while !name.empty? do
+      # add the student hash to the array
+      students << {name: name, cohort: :november}
+      puts "Now we have #{students.count} students"
+      # get another name from the user
+      name = gets.chomp
+    end
+    # return the array of students
+    students
+  end  
+
 students = [
     {name: "Dr. Hannibal Lecter", cohort: :november},
     {name: "Darth Vader", cohort: :november},
@@ -27,6 +46,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
