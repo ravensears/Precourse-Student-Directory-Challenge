d4b9cc5e6813a6251b82a8d4f6f9ea8ed0d59e7e
Raven Sears <ravenrosereynolds@gmail.com>
2021-12-03 14:31:02 +0000

    Student Directory Interactive Menu

--- directory.rb
+++ directory.rb
@@ -1,31 +1,63 @@
+@students = []
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  name = gets.chomp
+
+  while !name.empty? do
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    name = gets.chomp
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I dont know what you mean, try again"
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
 def print_header
     puts "The students of Villians Academy"
     puts "-------------"
   end
 
-def input_students
-    puts "Please enter the names of the students"
-    puts "To finish, just hit return twice"
-    # create an empty array
-    students = []
-    # get the first name
-    name = gets.chomp
-    # while the name is not empty, repeat this code
-    while !name.empty? do
-      # add the student hash to the array
-      students << {name: name, cohort: :november}
-      puts "Now we have #{students.count} students"
-      # get another name from the user
-      name = gets.chomp
-    end
-    # return the array of students
-    students
-  end  
+def print_students_list
+  @students.each do |student| 
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end  
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-print_header
-students = input_students
-print_footer(students)
+interactive_menu
